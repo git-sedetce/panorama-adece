4496ceec87d208d210f3c1a90d70d65ea80b34ac
Daniel Araújo <daniel.araujo@sedet.ce.gov.br>
2022-05-23 12:00:28 -0300

    de coeficiente-operacional para notícias

--- client/src/app/modules/noticias/noticias.component.ts
+++ client/src/app/modules/noticias/noticias.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-coeficiente-operacional",
-  templateUrl: "./coeficiente-operacional.component.html",
-  styleUrls: ["./coeficiente-operacional.component.scss"],
+  selector: "app-noticias",
+  templateUrl: "./noticias.component.html",
+  styleUrls: ["./noticias.component.scss"],
 })
 export class CoeficienteOperacionalComponent implements OnInit {
   constructor() {}
